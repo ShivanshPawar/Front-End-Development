747bccc2cc87d601702ac6f50634d8446fa4c1db
Shivansh Pawar <pawarshivansh93@gmail.com>
2025-07-13 11:31:38 +0530

    Front-End-Development

---
+++ Day37/Project-Exercise/AddFreind/script.js
@@ -0,0 +1,16 @@
+let btn = document.querySelector('#btn');
+let flw = document.querySelector('#followers')
+let isFollow = false;
+document.addEventListener('click', function () {
+    if (isFollow == false) {
+        flw.textContent = '1';
+        btn.textContent = 'following'
+        btn.style.backgroundColor = 'gray'
+        isFollow = true;
+    } else {
+        flw.textContent = '0';
+        btn.textContent = 'follow'
+        btn.style.backgroundColor = 'rgb(0, 136, 255)'
+        isFollow = false;
+    }
+});
